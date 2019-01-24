83ab822993f79f9e6786e5432df9a251b5c48983
designMoreWeb <divystolia@gmail.com>
2019-01-24 10:56:12 -0500

    connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+//user created
+import SongList from './SongList';
 
 const App = () =>{
-    return <div>App</div>;
+    return <div><SongList /></div>;
 };
 
 export default App;
