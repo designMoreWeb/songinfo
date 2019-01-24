4201789408cd5e415df47e8cf6c1f05bb84ed3d0
designMoreWeb <divystolia@gmail.com>
2019-01-24 11:24:13 -0500

    building the list with redux data

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 class SongList extends Component{
+    renderList(){
+        return this.props.songs.map((song)=>{
+            return(
+                <div className='item' key={song.title}>
+                    <div className='right floated content'>
+                        <button className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        });
+    }
     render(){
-        return <div>SongList</div>;
+        console.log(this.props);
+        return <div>{this.renderList()}</div>;
     }
 }
 
 //all of the data 
 const mapStateToProps = (state) =>{
-    console.log(state);
-    return state;
+    return { songs:state.songs }; 
 }
 
 export default connect(mapStateToProps)(SongList);
